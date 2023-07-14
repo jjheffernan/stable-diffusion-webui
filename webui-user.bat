6a1a6a83ee5471e28ec8bc4c3d26bd6f4682e96e
Hefftv StreamPC <jjheffernan99@gmail.com>
2023-07-14 02:16:10 -0400

    configured user batch file to run with optional args.

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\jjhef\AppData\Local\Programs\Python\Python311\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--xformers
+Rem available tags: "--xformers" current requires python 3.10
+Rem Uncomment the following to update the UI based off the fork on jjheffernan's repo
+:: git pull
 call webui.bat
